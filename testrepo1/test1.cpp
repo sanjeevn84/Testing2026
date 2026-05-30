3e26c2ed3677321f0b6599df1345175ebba6b8ec
sanjeev nayini <sanjeevnayini84@gmail.com>
2026-05-30 10:16:05 +0000

    first commit to github repo sample file

---
+++ testrepo1/test1.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	printf("\n first C++ program hello!");
+	//std::cout<<"first c++ prog"<<std::endl;
+	cout<<"\nfirst c++ prog"<<endl;
+	return 0;
+}
